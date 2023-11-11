2575e044835bee967d0e5004cc241ade23b60369
sp1rale <dimayaremchyk24@gmail.com>
2023-11-11 18:20:14 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	return 0;
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        ++start;
+        --end;
+    }
+}
+
+int main() {
+    const int size = 5;
+    int array[size] = { 1, 2, 3, 4, 5 };
+
+    std::cout << "Original Array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    reverseArray(array, size);
+
+    std::cout << "Reversed Array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
