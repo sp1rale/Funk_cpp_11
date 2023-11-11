9f269f4c512a0fd3ae970c68f04d0f685ec90abc
sp1rale <dimayaremchyk24@gmail.com>
2023-11-11 18:26:17 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
+bool isPrime(int num) {
+    if (num < 2) {
+        return false; 
+    }
+
+    int sqrtNum = static_cast<int>(sqrt(num));
+    for (int i = 2; i <= sqrtNum; ++i) {
+        if (num % i == 0) {
+            return false;  
+        }
+    }
+
+    return true;  
+}
+
+int countPrimes(const int arr[], int size) {
+    int count = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (isPrime(arr[i])) {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    const int size = 8;
+    int array[size] = { 2, 3, 5, 7, 10, 13, 15, 17 };
+
+    cout << "Array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    int primeCount = countPrimes(array, size);
+    cout << "Number of prime numbers: " << primeCount <<endl;
+
 	return 0;
 }
